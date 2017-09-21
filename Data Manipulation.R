830f2985c41af6ab213d15323f01dbafbfdd0966
shannondussoye <shannonduss@live.com>
2017-09-21 21:42:42 +1000

    files and script

--- Data Manipulation.R
+++ Data Manipulation.R
@@ -13,16 +13,31 @@ stations <- train_tap_on %>%
   unique()
 
 timestamp <- train_tap_on %>% 
-  select(datetime) %>% unique() 
+  select(datetime,date,time) %>% unique() 
 
 # timestamp <- timestamp %>% filter(!is.na(datetime))
 
-timestamp$datetime <- as.POSIXct(strptime(timestamp$datetime, "%Y%m%d %H:%M"))
-timestamp$datetime <- arrange(timestamp,-desc(datetime)) 
+# timestamp$datetime <- as.POSIXct(strptime(timestamp$datetime, "%Y%m%d %H:%M"))
+# timestamp$datetime <- arrange(timestamp,-desc(datetime)) 
 
 stations$j <- 1
 timestamp$j <- 1
 
 data <- inner_join(stations,timestamp,by='j') %>% select(-j)
 data <- arrange(data,datetime,loc)
-write.csv(data,"/home/shannon/Github/Opal/D3-data.csv",row.names = F)
+
+rm(stations)
+rm(timestamp)
+
+train_tap_on <- train_tap_on %>% select(-tap,-lat,-lon,-date,-time)
+data <- left_join(data,train_tap_on,by=c("datetime","loc"))
+data["count"][is.na(data["count"])] <- 0
+
+
+avg_count <- data %>% select(loc,date,count) %>% group_by(loc,date) %>% summarise(avg=mean(count)) 
+avg_count$avg <- round(avg_count$avg)
+
+d3_data <- inner_join(data,avg_count, by=c("loc","date"))
+d3_data$size <- ifelse(!d3_data$count, 0, d3_data$count / d3_data$avg)
+d3_data$size[is.infinite(d3_data$size) | is.nan(d3_data$size)] <- NA 
+write.csv(d3_data,"/home/shannon/Github/Opal/D3-data.csv",row.names = F)
